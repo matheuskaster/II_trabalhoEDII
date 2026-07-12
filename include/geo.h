337f10b693eca4aeec723ddb521b3030e287770d
matheuskaster <mathkaster@gmail.com>
2026-07-12 18:23:45 -0300

    12/07 [fix] Eu utilizava as cores depois no programa, com as quadras do geo, aí elas eram criadas na função main. Nesse programa, imagino que  não vai ser mais necessário, por isso, removi do .h do geo o parâmetro do ponteiro das Cores.

--- include/geo.h
+++ include/geo.h
@@ -18,6 +18,6 @@ de dados localizada atraves do gerenciador.
 ///@param quadras Estrutura que tem acesso a todas as informacoes para alterar os atributos das quadras.
 ///@param arq_geo Ponteiro do tipo arquivo, para acessar as informacoes das quadras.
 ///@param arq_svg Ponteiro do tipo arquivo, para poder desenhar as quadras.
-void geo (Gerenciador quadras, Cores cq, FILE* arq_geo, FILE* arq_svg);
+void geo (Gerenciador quadras, FILE* arq_geo, FILE* arq_svg);
 
 #endif
