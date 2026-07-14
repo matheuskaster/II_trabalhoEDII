4523f400eb1353620768c7949d82811d3616649e
matheuskaster <mathkaster@gmail.com>
2026-07-14 13:45:05 -0300

    14/07 Feita a base do meu qry, mais adaptação dos outros trabalhos. Lê o arquivo qry e encaminha passa os parâmetros para que ela seja executada em outro local

---
+++ include/qry.h
@@ -0,0 +1,19 @@
+//
+// Created by Matheus on 25/05/2026.
+//
+
+#ifndef QRY_H
+#define QRY_H
+#include <stdio.h>
+#include "quadra.h"
+
+/// @brief É uma função feita para poder ler o arquivo de entrada qry e chamar funções que irão executar os comandos.
+/// @param quadras É o gerenciador que tem acesso e controle ao hashfile, referente às quadras, do programa.
+/// @param grafo É o grafo com acesso às esquinas e ruas da cidade.
+/// @param file_qry É o arquivo de entrada do tipo .qry.
+/// @param file_txt É o arquivo de saída, com o relatório final, depois de ter sido processado pelo qry.
+/// @param file_svg É o arquivo de saída, com as ilustrações, depois de ter sido processado pelo qry.
+/// @param cq É o ponteiro que aponta para a estrutura que possue todas as cores das quadras.
+void qry (Gerenciador quadras, Grafo grafo, FILE* file_qry, FILE* file_txt, FILE* file_svg, Cores cq);
+
+#endif
---
+++ src/qry.c
@@ -0,0 +1,71 @@
+//
+// Created by Matheus on 25/05/2026.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../include/hash.h"
+#include "../include/grafo.h"
+#include "../include/qry.h"
+#include "../include/svg.h"
+
+void qry (Gerenciador quadras, Grafo grafo, FILE* file_qry, FILE* file_txt, FILE* file_svg, Cores cq) {
+
+    char linha[512];
+    char comando[6];
+
+    abre_svg(file_svg);
+
+    while (fgets(linha, sizeof(linha), file_qry) != NULL) {
+        if(linha[0] == '\n' || linha[0] == '\r'){
+            continue;
+        }
+
+        sscanf(linha, "%s", comando);
+
+        if (strcmp(comando, "@o?") == 0) {
+            char* reg = NULL;
+            char cep[10];
+            char face = '\0';
+            int num;
+            sscanf(linha, "%s %s %c %d", reg, cep, &face, &num);
+            o(reg, cep, face, num, file_svg, file_txt);
+        }
+        else if (strcmp(comando, "mvm") == 0) {
+            double v = 0.0, x = 0.0, y = 0.0, w = 0.0, h = 0.0;
+            sscanf(linha, "%lf %lf %lf %lf %lf", &v, &x, &y, &w, &h);
+            mvm(grafo, x, y, w, h);
+        }
+        else if (strcmp(comando, "regs") == 0) {
+            double vl = 0.0;
+            sscanf(linha, "%lf", &vl);
+            regs(grafo, vl, file_svg, file_txt);
+        }
+        else if (strcmp(comando, "exp") == 0) {
+            double vl = 0.0;
+            sscanf(linha, "%lf", &vl);
+            exp(grafo, vl, file_svg);
+        }
+        else if (strcmp(comando, "p?") == 0) {
+            char *reg1 = NULL, *reg2 = NULL, *cc = NULL, *cr = NULL;
+            sscanf(linha, "%s %s %s %s", reg1, reg2, cc, cr);
+            p(grafo, reg1, reg2, cc, cr, file_svg, file_txt);
+        }
+    }
+    int total;
+    Registro* vetor_quadras = pega_todos_registros(quadras, &total);
+    for (int i = 0; i < total; i++) {
+        Registro r = vetor_quadras[i];
+
+        char* cep = get_chave_registro(r);
+        char* dados = get_dados_registro(r);
+        Quadra q = reconstroi_quadra(cep, dados);
+        desenha_quadra_svg(file_svg, q, cq);
+
+        libera_registro(r);
+        libera_quadra(q);
+    }
+    free(vetor_quadras);
+    fecha_svg(file_svg);
+}
