7a78c62237808aee83f8e0903da04925612f0d4c
matheuskaster <mathkaster@gmail.com>
2026-07-15 20:51:47 -0300

    15/07 [fix] Tava dando conflito de declaração, porque eu esqueci de passar a estrutura que guarda os registradores, também

--- src/mobilidade.c
+++ src/mobilidade.c
@@ -199,6 +199,9 @@ void expande(Grafo g, double vl, FILE* file_svg) {
 void p(Grafo g, Registradores r, char* reg1, char* reg2, char* cc, char* cr, FILE* file_svg, FILE* file_txt) {
     double x1, y1, x2, y2;
 
+    busca_registrador(r, reg1, &x1, &y1);
+    busca_registrador(r, reg2, &x2, &y2);
+
     int u_origem = encontra_vertice_mais_proximo(g, x1, y1);
     int u_destino = encontra_vertice_mais_proximo(g, x2, y2);
 
